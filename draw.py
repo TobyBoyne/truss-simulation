ca07cb59763b8fe705e61635b659c37c45724a87
Toby Boyne <tobyboyne@gmail.com>
2020-04-19 17:04:54 +0100

    Moved on click event logic to JointHandler

--- draw.py
+++ draw.py
@@ -6,28 +6,27 @@ from parts import Joint
 class JointHandler:
 	def __init__(self, ax):
 		self.ax = ax
-		
+		self.joints = []
+
+	def __call__(self, event):
+		"""On click event is handled by the JointHandler class"""
+		pos = np.array([event.xdata, event.ydata])
+		plt.plot(*pos, 'o')
+		self.joints.append(Joint(pos))
+		fig.canvas.draw()
 
 
 def get_draw_ui():
 	"""Creates figure and axes for drawing
 	Connects click event to onclick function"""
 	fig, ax = plt.subplots()
-	joints = JointHandler(ax)
-	fig.canvas.mpl_connect('button_press_event', joints)
+	handler = JointHandler(ax)
+	fig.canvas.mpl_connect('button_press_event', handler)
 	ax.autoscale(False)
-	return fig, ax, joints
+	return fig, ax, handler
 
-
-def on_click(event):
-	print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-		  (event.button, event.x, event.y, event.xdata, event.ydata))
-
-	plt.plot(event.xdata, event.ydata, 'o')
-
-	fig.canvas.draw()
 
 
 if __name__ == '__main__':
-	fig, ax, joints = get_draw_ui()
+	fig, ax, handler = get_draw_ui()
 	plt.show()
