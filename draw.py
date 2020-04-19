11db8df97099023683e758d98e1620a7d9cb6bad
Toby Boyne <tobyboyne@gmail.com>
2020-04-19 16:59:33 +0100

    Created JointHandler class

--- draw.py
+++ draw.py
@@ -1,23 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from parts import Joint
+
+class JointHandler:
+	def __init__(self, ax):
+		self.ax = ax
+		
+
+
 def get_draw_ui():
 	"""Creates figure and axes for drawing
 	Connects click event to onclick function"""
 	fig, ax = plt.subplots()
-	fig.canvas.mpl_connect('button_press_event', on_click)
+	joints = JointHandler(ax)
+	fig.canvas.mpl_connect('button_press_event', joints)
 	ax.autoscale(False)
-	return fig, ax
+	return fig, ax, joints
 
 
 def on_click(event):
 	print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
 		  (event.button, event.x, event.y, event.xdata, event.ydata))
 
-	plt.plot(event.xdata, event.ydata, 'x')
+	plt.plot(event.xdata, event.ydata, 'o')
 
 	fig.canvas.draw()
 
 
 if __name__ == '__main__':
-	fig, ax = get_draw_ui()
+	fig, ax, joints = get_draw_ui()
+	plt.show()
