e52ce3da6209ac240b640d888fbf0a2ffa40b0f7
Toby Boyne <tobyboyne@gmail.com>
2020-04-20 16:35:28 +0100

    Added on_release method to create members between joints

--- draw.py
+++ draw.py
@@ -9,7 +9,7 @@ class JointHandler:
 		self.fig = fig
 		self.ax = ax
 		self.joints = []
-		self.mouse_down = False
+		self.origin_joint = None
 
 	def on_click(self, event):
 		"""On click event is handled by the JointHandler class
@@ -20,9 +20,10 @@ class JointHandler:
 		for joint in self.joints:
 			if joint.is_near(pos):
 				if event.button == MouseButton.LEFT:
-					self.mouse_down = True
+					self.origin_joint = joint
 				elif event.button == MouseButton.RIGHT:
 					joint.change_joint_type()
+					joint.draw(self.ax)
 				break
 		# else triggers if no break - click coord is not close to existing point
 		else:
@@ -30,7 +31,22 @@ class JointHandler:
 				new_joint = Joint(pos)
 				self.joints.append(new_joint)
 				new_joint.draw(self.ax)
-				self.fig.canvas.draw()
+		self.fig.canvas.draw()
+
+	def on_release(self, event):
+		"""If the mouse is being held down to draw a new member, create a member between the origin
+		and the nearest joint, if one is near"""
+		if self.origin_joint is not None:
+			print("!")
+			pos = np.array([event.xdata, event.ydata])
+			for joint in self.joints:
+				if joint.is_near(pos):
+					joint.add_member(self.origin_joint)
+					xs, ys = zip(joint.pos, self.origin_joint.pos)
+					plt.plot(xs, ys)
+
+			self.origin_joint = None
+			self.fig.canvas.draw()
 
 
 def get_draw_ui():
@@ -39,6 +55,7 @@ def get_draw_ui():
 	fig, ax = plt.subplots()
 	handler = JointHandler(fig, ax)
 	fig.canvas.mpl_connect('button_press_event', handler.on_click)
+	fig.canvas.mpl_connect('button_release_event', handler.on_release)
 	ax.autoscale(False)
 	return fig, ax, handler
 
