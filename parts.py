ef30f7dc411256c391715579054210401780474b
Toby Boyne <tobyboyne@gmail.com>
2020-04-20 16:03:19 +0100

    Added different support types

--- parts.py
+++ parts.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# support types show the (X, Y, rot) restrictions
+SUPPORT_TYPES = {
+	0: (False, False, False),	# pin-joint
+	1: (True, False, False),	# X-restricted roller support
+	2: (False, True, False),	# Y-restricted roller support
+	3: (True, True, False),		# stationary pin-joint support
+	4: (True, True, True)		# cantilever support
+}
+
 class Joint:
 	"""Class to contain a joint or support
 	Handles all members as connections between joints"""
@@ -13,10 +22,10 @@ class Joint:
 		self.connections.append(other)
 
 	def draw(self, ax: plt.Axes):
-		ax.plot(*self.pos, 'o')
+		ax.plot(*self.pos,'o', color="red")
 
 	def is_near(self, other_pos, radius=10):
 		return np.linalg.norm(self.pos - other_pos) < radius
 
 	def change_joint_type(self):
-		self.support_type += 1
+		self.support_type = (self.support_type + 1) % len(SUPPORT_TYPES)
