5c9ce7518cc9d0788d7fa25ee40d737d3edba858
Toby Boyne <tobyboyne@gmail.com>
2020-04-20 14:36:03 +0100

    Use draw method in Joint class to plot new joints

--- draw.py
+++ draw.py
@@ -4,23 +4,25 @@ import numpy as np
 from parts import Joint
 
 class JointHandler:
-	def __init__(self, ax):
+	def __init__(self, fig, ax):
+		self.fig = fig
 		self.ax = ax
 		self.joints = []
 
 	def __call__(self, event):
 		"""On click event is handled by the JointHandler class"""
 		pos = np.array([event.xdata, event.ydata])
-		plt.plot(*pos, 'o')
-		self.joints.append(Joint(pos))
-		fig.canvas.draw()
+		new_joint = Joint(pos)
+		self.joints.append(new_joint)
+		new_joint.draw(self.ax)
+		self.fig.canvas.draw()
 
 
 def get_draw_ui():
 	"""Creates figure and axes for drawing
 	Connects click event to onclick function"""
 	fig, ax = plt.subplots()
-	handler = JointHandler(ax)
+	handler = JointHandler(fig, ax)
 	fig.canvas.mpl_connect('button_press_event', handler)
 	ax.autoscale(False)
 	return fig, ax, handler
