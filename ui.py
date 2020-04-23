1455335f713679769a055321f953b9771f10d3a5
Toby Boyne <tobyboyne@gmail.com>
2020-04-23 13:36:11 +0100

    Added mode property to choose function

--- ui.py
+++ ui.py
@@ -3,9 +3,16 @@ from matplotlib.backend_bases import MouseButton, MouseEvent, KeyEvent
 from matplotlib.widgets import RadioButtons
 import numpy as np
 from typing import Tuple
+from enum import Enum
 
 from parts import Joint, Member
 from structure import Structure
+
+class Modes(Enum):
+	DRAW = 0
+	FORCE = 1
+	DELETE = 2
+
 
 class EventHandler:
 	"""Handles all events from user input
@@ -18,13 +25,13 @@ class EventHandler:
 		self.origin_joint = None
 		self.new_line, = self.ax.plot([], [], lw=3, visible=False, ls='--')
 
-		self.mode_buttons = RadioButtons(rax, ('Draw', 'Force', 'Delete'))
-		self.mode_buttons.on_clicked(self.choose_mode)
-		self.mode = 'Draw'
+		self.mode_buttons = RadioButtons(rax, [name for name in Modes.__members__])
 
 
-	def choose_mode(self, label):
-		self.mode = label
+	@property
+	def mode(self):
+		mode = Modes[self.mode_buttons.value_selected]
+		return mode
 
 	def on_click(self, event: MouseEvent):
 		"""On click event is handled by the JointHandler class
@@ -32,30 +39,48 @@ class EventHandler:
 		If it is double-clicked, remove the joint
 		If the user click-and-drags from an existing joint, draw a new member
 		Otherwise, add a new joint"""
+		# if the click is not in the drawing axes, ignore the event
+		if event.inaxes is not self.ax:
+			return
 		pos = np.array([event.xdata, event.ydata])
 		joint = self.structure.get_nearest_joint(pos)
-		if joint is not None:
-			if event.button == MouseButton.LEFT:
-				if event.dblclick:
-					# delete the joint
-					self.structure.delete_joint(joint)
-				else:
-					# set the new member to be visible
-					self.origin_joint = joint
-					self.new_line.set_data(*zip(joint.pos, pos))
-					self.new_line.set_visible(True)
 
-			elif event.button == MouseButton.RIGHT:
-				# change the type of support
-				joint.change_joint_type()
-				joint.draw(self.ax)
+		# DRAW
+		if self.mode == Modes.DRAW:
+			if joint is not None:
+				if event.button == MouseButton.LEFT:
+					if event.dblclick:
+						# delete the joint
+						self.structure.delete_joint(joint)
+					else:
+						# set the new member to be visible
+						self.origin_joint = joint
+						self.new_line.set_data(*zip(joint.pos, pos))
+						self.new_line.set_visible(True)
 
-		# else triggers if click coord is not close to existing point
+				elif event.button == MouseButton.RIGHT:
+					# change the type of support
+					joint.change_joint_type()
+					joint.draw(self.ax)
+
+			# else triggers if click coord is not close to existing point
+			else:
+				if event.button == MouseButton.LEFT:
+					# create new joint
+					new_joint = self.structure.add_joint(pos)
+					new_joint.draw(self.ax)
+
+		# FORCE
+		elif self.mode == Modes.FORCE:
+			pass
+
+		elif self.mode == Modes.DELETE:
+			pass
+
+		# other modes not implemented
 		else:
-			if event.button == MouseButton.LEFT:
-				# create new joint
-				new_joint = self.structure.add_joint(pos)
-				new_joint.draw(self.ax)
+			raise NotImplementedError
+
 		self.fig.canvas.draw()
 
 	def on_release(self, event: MouseEvent):
